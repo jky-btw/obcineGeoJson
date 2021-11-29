1d3e293067b4fefb89997c99d71278bfa245b4b4
Jaka <jakaskerjanc@gmail.com>
2021-11-29 09:52:41 +0100

    inital commit

---
+++ split-multi.py
@@ -0,0 +1,16 @@
+# https://github.com/stefanb/gurs-obcine
+
+import json
+
+js = open('OB.geojson', 'r').read()
+gj = json.loads(js)
+
+for feature in gj['features']:
+
+    xfeature = {"type": "Feature", "properties": {},
+                "geometry": {"type": "Polygon"}}
+    xfeature['geometry']['coordinates'] = feature['geometry']['coordinates']
+    output = {"type": "FeatureCollection", "features": [xfeature]}
+    name = "{mid}.geojson".format(
+        mid=feature["properties"]["OB_MID"], name=feature["properties"]["OB_UIME"])
+    open("data/"+name, 'w').write(json.dumps(output))
